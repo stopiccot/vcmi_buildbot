d1906d6999613452924814fb437dae99d5a9ad0d
stopiccot <alexey.petruchik@gmail.com>
2014-04-10 01:36:07 +0300

    return to own osx box + fix for Xcode 5.1

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,12 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.require_version ">= 1.5.0"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+    
     # config.vm.box = "stopiccot/osx-10.9-xcode"
-    config.vm.box = "AndrewDryga/osx-mavericks"
+    # config.vm.box = "AndrewDryga/osx-mavericks"
+
+    config.vm.box = "osx-10.9-xcode"
+    config.vm.box_url = "http://stopiccot.com/osx-10.9-xcode.box"
+
     config.vm.provision "shell", path: "osx_mavericks_setup.sh"
 end
--- osx_mavericks_setup.sh
+++ osx_mavericks_setup.sh
@@ -3,12 +3,12 @@ curl -o get-pip.py https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
 rm get-pip.py
 
-Configure buildslave
+# Configure buildslave
 pip install virtualenv
 mkdir buildslave
 cd buildslave
 virtualenv --no-site-packages sandbox
 source sandbox/bin/activate
-pip install buildbot-slave
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future pip install buildbot-slave
 buildslave create-slave slave 10.0.2.2:9989 example-slave pass
 buildslave start slave
